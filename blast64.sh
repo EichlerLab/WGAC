8601460f8ca9f7a5b4a2e9d830dc72a2979ad63b
John Huddleston <jlhudd@uw.edu>
2012-01-25 10:50:07 -0800

    updated blast SGE script to work with openmpi

--- blast64.sh
+++ blast64.sh
@@ -1,34 +1,41 @@
-#! /bin/sh
+#!/bin/bash
 
 # Run BLAST64 for the given input sequences.
 #
 # Usage: qsub -q all.q blast64.sh -i <INPUT> -o <OUTPUT> -b <BLASTDB>
 
 # Specify the shell for this job
-#$ -S /bin/sh
+#$ -S /bin/bash
 
 export MPICH_PROCESS_GROUP=no 
 export P4_RSHCOMMAND=/usr/bin/rsh 
 export JOBDIR=$TMPDIR/work
 
-#$ -pe mpich 40
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib64/openmpi/1.4-gcc/lib
+export PATH=$PATH:/usr/lib64/openmpi/1.4-gcc/bin
+export PATH=/net/eichler/vol7/home/psudmant/local_installations/bin:$PATH
+export MANPATH=$MANPATH:/usr/lib64/openmpi/1.4-gcc/man
+export PYTHONPATH=/net/gs/vol2/home/psudmant/local_installations/lib/python2.6/site-packages:$PYTHONPATH
+
+#$ -pe orte 10-30
 
 # Send an email when the script begins, ends, aborts, or suspends.
 #$ -m beas
 
-# The job is located in the current
-# working directory.
+#$ -l h_vmem=8G
+#$ -l disk_free=4G
+
+# The job is located in the current working directory.
 #$ -cwd
 
 echo "Got $NSLOTS slots"
 echo "path=$PATH"
 echo "P4_RSHCOMMAND=$P4_RSHCOMMAND"
-echo "machine_file=$TMPDIR/machines"
 
 WORKING_DIR=$SGE_O_WORKDIR
-INPUT=$WORKING_DIR/fugu
-OUTPUT=$WORKING_DIR/blastout
-DATABASE_PATH=$WORKING_DIR/blastdb/bofugu
+INPUT="$WORKING_DIR/fugu"
+OUTPUT="$WORKING_DIR/blastout"
+DATABASE_PATH="$WORKING_DIR/blastdb/bofugu"
 BLAST_TMP_DIR=/var/tmp/blastdb
 
 # Get options from the user.
@@ -67,19 +74,25 @@ echo Tmp path: $BLAST_TMP_DIR
 echo Input: $INPUT
 echo Output: $OUTPUT
 
-# Create tmp directory on cluster nodes.
-/usr/bin/uniq $TMPDIR/machines | /net/eichler/vol2/local/bin/rgang - mkdir -p $BLAST_TMP_DIR
-
-# Copy BlastDB files to nodes.
-/usr/bin/uniq $TMPDIR/machines | /net/eichler/vol2/local/bin/rgang --rcpt 8000  -c --rcp="/usr/bin/rcp -r"  - $DATABASE_PATH.[^q]* $BLAST_TMP_DIR
+echo Copying files to nodes
+# Create tmp directory on cluster nodes and copy BlastDB files to nodes.
+mpirun -mca btl ^openib -np $NSLOTS \
+  /net/eichler/vol4/home/jlhudd/src/rsync_mpi/rsync_mpi.py \
+  "$DATABASE_PATH.[^q]*" "$BLAST_TMP_DIR" \
+  --pre_sync_commands="mkdir -p $BLAST_TMP_DIR"
 
 # Run BLAST.
-#/net/eichler/vol6/software/mpich-1.2.7-i386/bin/mpirun -np $NSLOTS -machinefile $TMPDIR/machines /net/eichler/vol7/home/ginger/bin/general_pipe "/net/eichler/vol2/local/bin/blastall -p blastn -i dummy_in -o dummy_out  -d $BLAST_TMP_DIR/$DATABASE_NAME -G 180 -E 1 -q -80 -r 30 -e 1e-30 -W 28"   $INPUT    $OUTPUT ".bo" $JOBDIR
+echo Running BLAST
+mpirun -mca btl ^openib \
+  -np $NSLOTS \
+  /net/eichler/vol4/home/jlhudd/bin/general_pipe \
+  "/net/eichler/vol2/local/bin/blastall -p blastn -i dummy_in -o dummy_out -d $BLAST_TMP_DIR/$DATABASE_NAME -v 5000 -b 5000 -G 180 -E 1 -q -80 -r 30 -e 1e-30 -F F -z 3000000000 -Y 3000000000" \
+  $INPUT $OUTPUT ".bo" $TMPDIR
 
-/net/eichler/vol6/software/mpich-1.2.7-i386/bin/mpirun -np $NSLOTS -machinefile $TMPDIR/machines /net/eichler/vol7/home/ginger/bin/general_pipe "/net/eichler/vol2/local/bin/blastall -p blastn -i dummy_in -o dummy_out  -d $BLAST_TMP_DIR/$DATABASE_NAME -z 3000000000 -Y 3000000000 -v 5000 -b 5000 -G 180 -E 1 -q -80 -r 30 -e 1e-30 -F F"   $INPUT    $OUTPUT ".bo" $JOBDIR
+echo Done
 
-# Create tmp directory on cluster nodes.
-/usr/bin/uniq $TMPDIR/machines | /net/eichler/vol2/local/bin/rgang - rm -rf $BLAST_TMP_DIR
+# Clean up.
+#/usr/bin/uniq $TMPDIR/machines | /net/eichler/vol2/local/bin/rgang - rm -rf $BLAST_TMP_DIR
 
 
 # blastall is v2.2.11, not much different than 2.2.20
