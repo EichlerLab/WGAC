ddb8feae53b64f246cd45c6633d614f051547259
david <dgordon@uw.edu>
2019-07-18 11:15:15 -0700

    run_lastz_self.snake.sh to use miniconda/4.5.12

--- run_lastz_self.snake.sh
+++ run_lastz_self.snake.sh
@@ -1,4 +1,4 @@
 mkdir -p log
 module purge
-module load modules modules-init modules-gs/prod modules-eichler/prod anaconda/201710
+module load modules modules-init modules-gs/prod modules-eichler/prod miniconda/4.5.12
 snakemake -s lastz_self.snake --drmaa " -q eichler-short.q -l h_rt=24:00:00  -V -cwd -e ./log -o ./log {params.sge_opts}  -S /bin/bash" -w 100 --jobs 100 -p
