0cea21982a622b7b16c31ebb6a805b6af7368ec5
david <dgordon@uw.edu>
2019-08-28 16:17:15 -0700

    make_oo.weild10kb.join.all.cull.sh makefile_wgac getting rid of more jlhudd

--- make_oo.weild10kb.join.all.cull.sh
+++ make_oo.weild10kb.join.all.cull.sh
@@ -9,6 +9,6 @@ INPUT="$1"
 OUTPUT="$2"
 
 cd data
-table_line_culler2.pl -p oo.weild10kb.pieces.all -t'$c[27]>=0.90 && $c[64]>=1000'
-table_line_culler2.pl -p oo.weild10kb.join.all -t'$c[27]>=0.90 && $c[22]>=1000'
+../table_line_culler2.pl -p oo.weild10kb.pieces.all -t'$c[27]>=0.90 && $c[64]>=1000'
+../table_line_culler2.pl -p oo.weild10kb.join.all -t'$c[27]>=0.90 && $c[22]>=1000'
 cd ../
