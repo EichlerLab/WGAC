e349c0a5b395dc77e4c0078f4f24938116c9588a
david <dgordon@uw.edu>
2020-05-28 17:22:56 -0700

    run_blast_fugu.snake.sh to require this be run on centos6

--- run_blast_fugu.snake.sh
+++ run_blast_fugu.snake.sh
@@ -5,4 +5,4 @@ module load miniconda/4.5.12
 
 
 
-snakemake -s blast_fugu.snake  --jobname "{rulename}.{jobid}" --drmaa " -V -cwd -e ./log -o ./log {params.sge_opts}  -S /bin/bash -l testing=true" -w 100 --jobs 100 -p -k --restart-times 1
+snakemake -s blast_fugu.snake  --jobname "{rulename}.{jobid}" --drmaa " -V -cwd -e ./log -o ./log {params.sge_opts}  -S /bin/bash " -w 100 --jobs 100 -p -k --restart-times 1
