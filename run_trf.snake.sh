21b8fc6afc936b147aad3b300f1710ee59698240
david <dgordon@uw.edu>
2020-07-10 14:47:29 -0700

    run_global_align.snake.sh run_global_align_110k.snake.sh run_lastz_self.snake.sh run_trf.snake.sh run_trim_ends.snake.sh to remove testing=true so runs on centos7

--- run_trf.snake.sh
+++ run_trf.snake.sh
@@ -4,4 +4,4 @@ module load modules modules-init modules-gs/prod modules-eichler/prod
 
 module load miniconda/4.5.12
 snakemake -s trf.snake populate_trf_with_links
-snakemake -s trf.snake --jobname "{rulename}.{jobid}"  --drmaa " -l testing=true -V -cwd -w n -e ./log -o ./log {params.sge_opts} -S /bin/bash" -j 100 -k --rerun
+snakemake -s trf.snake --jobname "{rulename}.{jobid}"  --drmaa " -V -cwd -w n -e ./log -o ./log {params.sge_opts} -S /bin/bash" -j 100 -k --rerun
--- run_trim_ends.snake.sh
+++ run_trim_ends.snake.sh
@@ -5,4 +5,4 @@ module load miniconda/4.5.12
 
 
 
-snakemake -s trim_ends.snake  --jobname "{rulename}.{jobid}" --drmaa " -V -cwd -e ./log -o ./log {params.sge_opts}  -S /bin/bash -l testing=true" -w 100 --jobs 100 -p -k
+snakemake -s trim_ends.snake  --jobname "{rulename}.{jobid}" --drmaa " -V -cwd -e ./log -o ./log {params.sge_opts}  -S /bin/bash" -w 100 --jobs 100 -p -k
