b2d18b2b8a9717b10967ee24dab07a5591367a11
david <dgordon@uw.edu>
2020-08-11 12:57:45 -0700

    run_blast_fugu.snake.sh run_global_align.snake.sh run_global_align_110k.snake.sh run_lastz_self.snake.sh run_trf.snake.sh run_trim_ends.snake.sh to add -w n so don't get no suitable queues error

--- run_global_align.snake.sh
+++ run_global_align.snake.sh
@@ -7,4 +7,4 @@ module load miniconda/4.5.12
 
 
 
-snakemake -s global_align.snake  --jobname "{rulename}.{jobid}" --drmaa " -V -cwd -e ./log -o ./log {params.sge_opts}  -S /bin/bash " -w 100 --jobs 100 -p -k
+snakemake -s global_align.snake  --jobname "{rulename}.{jobid}" --drmaa " -w n -V -cwd -e ./log -o ./log {params.sge_opts}  -S /bin/bash " -w 100 --jobs 100 -p -k
