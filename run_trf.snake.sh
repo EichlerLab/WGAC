0b596f78ffc8088cc8551fb2f8cd2fd60dc2e4a9
david <dgordon@uw.edu>
2022-02-09 14:29:10 -0800

    run_trf.snake.sh to add --restart-times

--- run_trf.snake.sh
+++ run_trf.snake.sh
@@ -4,4 +4,9 @@ module load modules modules-init modules-gs/prod modules-eichler/prod
 
 module load miniconda/4.5.12
 snakemake -s trf.snake populate_trf_with_links
-snakemake -s trf.snake --jobname "{rulename}.{jobid}"  --drmaa " -w n -V -cwd -w n -e ./log -o ./log {params.sge_opts} -S /bin/bash" -j 100 -k --rerun
+
+# added Feb 9, 2022 (DG):
+# restart-times was necessary since occasionally got this error:
+# Error recording metadata for finished job ([Errno 2] No such file or directory: 'fugu_trf/chr2_274.fugu'). Please ensure write permissions for the directory /net/eichler/vol27/projects/hprc/nobackups/chm13v2_wgac3/.snakemake
+
+snakemake -s trf.snake --jobname "{rulename}.{jobid}"  --drmaa " -w n -V -cwd -w n -e ./log -o ./log {params.sge_opts} -S /bin/bash" -j 100 -k --rerun --restart-times 1
