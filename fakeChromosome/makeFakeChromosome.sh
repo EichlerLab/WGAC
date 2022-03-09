3b39ee42324bc0f13382ec1a1674f1b21967d82d
david <dgordon@uw.edu>
2022-03-08 16:41:25 -0800

    fakeChromosome files

--- fakeChromosome/makeFakeChromosome.sh
+++
@@ -1,2 +0,0 @@
-cat fastalength.log | sed 1d | sed '$d' | grep -vw -f chromosomes.txt | sort -k3,3nr | awk 'BEGIN {x = 0} {print $2, $3, x; x += $3}' >fakeChromosomeOffsets.txt
-
--- fakeChromosome/run_convert_xw.al.py.sh
+++
@@ -1,1 +0,0 @@
-./convert_xw.al.py --szOffsetTable fakeChromosomeOffsets.txt --xm_al xw.al --szOutputxm_al fake_chromosome_xw.al
