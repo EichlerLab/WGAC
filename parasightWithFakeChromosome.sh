83e0f26bd084ebb0e4f1465d374cbbb808745cfe
david <dgordon@uw.edu>
2022-06-02 17:31:54 -0700

    parasightWithFakeChromosome.sh globalViewFakeChromosome/for_parasight_fake_chromosome.sh so that the png files are only 1 subdirectory below the main instead of 2

--- parasightWithFakeChromosome.sh
+++ parasightWithFakeChromosome.sh
@@ -11,7 +11,7 @@ ln -s ../globalview10k.pst
 run_convert_xw.al.py.sh
 module load perl/5.14.2 && for_parasight_fake_chromosome.sh
 
-cd globalViewFakeChromosome
+#cd globalViewFakeChromosome
 
 test -f global_view_5k_90.png -a -f global_view_5k_95.png -a -f global_view_5k_98.png -a -f global_view_10k_90.png -a -f global_view_10k_95.png -a -f global_view_10k_98.png -a -f global_view_20k_90.png -a -f global_view_20k_95.png -a -f global_view_20k_98.png && touch ../../parasightWithFakeChromosomeDone
 
